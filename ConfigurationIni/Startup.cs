2e3b3d6b5e8f3847e2f9b08d0a4a2b3dd585cb76
Leonardo Queres <leonardoqueres@hotmail.com>
2018-09-18 15:11:18 -0300

    Add files via upload

---
+++ ConfigurationIni/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace ConfigurationIni
+{
+    public class Startup
+    {
+        public IConfigurationRoot _config;
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder();
+            builder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddIniFile("Settings.ini");
+
+            _config = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                foreach (var c in _config.AsEnumerable())
+                {
+                    await context.Response.WriteAsync($"{c.Key} {c.Value}\n");
+                }
+            });
+        }
+    }
+}
